4e3ad603e9a05c0c551ca009b03ceeed78cf4a21
kumarshantanu01 <shantanuk280@gmail.com>
2021-08-17 21:52:03 +0530

    blog-website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
+import { Container, createTheme, Grid, ThemeProvider, makeStyles } from '@material-ui/core';
 import './App.css';
+import { FeaturedPost } from './components/FeaturedPost';
+import Footer from './components/Footer';
+import { Header } from './components/Header';
+import Main from './components/Main';
+import { PostCard } from './components/PostCard';
+import Sidebar from './components/Sidebar';
+import { featuredPosts, sidebar } from './data/Data';
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 function App() {
+
+  const darkTheme = createTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  const classes = useStyles();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <Container>
+      <Header/>
+      <FeaturedPost/>
+      <br/>
+      <Grid container spacing={4}>
+        {featuredPosts.map((post) => (
+          <PostCard post={post} key={post.title}/>
+        ))}
+      </Grid>
+      <Grid container spacing={5} className={classes.mainGrid}>
+        <Main title="hello world"/>
+        <Sidebar 
+          title={sidebar.title}
+          description={sidebar.description}
+          archives={sidebar.archives}
+          social={sidebar.social}
+        />
+      </Grid>
+    </Container>
+    <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+    />
+    </ThemeProvider>
   );
 }
 
